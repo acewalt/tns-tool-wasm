ae77dcbaa95a065554d80620e1cf883126ec96d1
walt_to_art <guard.mauricio.save@gmail.com>
2026-08-27 08:03:02 -0500

    Load unified localized drag-drop UI

--- multi-image-upload.js
+++ multi-image-upload.js
@@ -1,10 +1,17 @@
 (() => {
   "use strict";
 
-  if (document.querySelector('script[data-media-import-controller="true"]')) return;
+  if (!document.querySelector('script[data-media-import-controller="true"]')) {
+    const mediaScript = document.createElement("script");
+    mediaScript.src = "./media-import-controller.js?v=20260827-media-import-v3";
+    mediaScript.dataset.mediaImportController = "true";
+    document.head.appendChild(mediaScript);
+  }
 
-  const script = document.createElement("script");
-  script.src = "./media-import-controller.js?v=20260827-media-import-v3";
-  script.dataset.mediaImportController = "true";
-  document.head.appendChild(script);
+  if (!document.querySelector('script[data-drag-drop-ui="true"]')) {
+    const dragUiScript = document.createElement("script");
+    dragUiScript.src = "./drag-drop-ui.js?v=20260827-drag-drop-v1";
+    dragUiScript.dataset.dragDropUi = "true";
+    document.head.appendChild(dragUiScript);
+  }
 })();
