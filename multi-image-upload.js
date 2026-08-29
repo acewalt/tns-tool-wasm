f887c588465719b5ab3c5a90df56dcf90abd42e2
walt_to_art <guard.mauricio.save@gmail.com>
2026-08-28 19:17:08 -0500

    Load cross-origin isolation wrapper before Ndless build UI

--- multi-image-upload.js
+++ multi-image-upload.js
@@ -30,7 +30,7 @@
     });
   }
 
-  const V="20260828-ndless-project-v6";
+  const V="20260829-ndless-project-v7";
   ensureStyle('link[data-runtime-loading-overlay-style="true"]', "./runtime-loading-overlay.css?v=20260827-runtime-loader-v3", "runtimeLoadingOverlayStyle");
   ensureStyle('link[data-header-controls-compact="true"]', "./header-controls-compact.css?v=20260827-header-controls-v3", "headerControlsCompact");
   ensureStyle('link[data-tns-type-progress-style="true"]', `./tns-type-detection-progress.css?v=${V}`, "tnsTypeProgressStyle");
@@ -82,6 +82,7 @@
     .then(() => loadScript('script[data-ndless-project-sdk-enhancements-v2="true"]', `./ndless-project-sdk-enhancements-v2.js?v=${V}`, "ndlessProjectSdkEnhancementsV2"))
     .then(() => loadScript('script[data-ndless-project-file-tree="true"]', `./ndless-project-file-tree.js?v=${V}`, "ndlessProjectFileTree"))
     .then(() => loadScript('script[data-ndless-build-manager="true"]', `./ndless-build-manager.js?v=${V}`, "ndlessBuildManager"))
+    .then(() => loadScript('script[data-ndless-build-coi="true"]', `./ndless-build-coi.js?v=${V}`, "ndlessBuildCoi"))
     .then(() => loadScript('script[data-ndless-build-ui="true"]', `./ndless-build-ui.js?v=${V}`, "ndlessBuildUi"));
   window.TnsRuntimeReady = tnsChain;
   window.NdlessRuntimeReady = tnsChain;
